fe983ce8bef0fbf5eaed5e5584c8094f4dda6cbb
Yasser Zadeh <yassermzh@gmail.com>
2019-07-28 16:42:25 +0200

    add story

--- src/components/home.js
+++ src/components/home.js
@@ -1,34 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Jumbotron from "react-bootstrap/Jumbotron"
-import Layout from "../components/layout"
+import Layout from "./layout"
+import Story from "./story"
 
-const Home = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query Home {
-      site {
-        siteMetadata {
-          title
-          description
-        }
-      }
-    }
-  `)
-
-  return (
-    <Layout>
-      <Jumbotron fluid className="d-flex justify-content-center">
-        <p className="lead align-self-center">
-          {data.site.siteMetadata.description}
-        </p>
-      </Jumbotron>
-    </Layout>
-  )
-}
-
-Home.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+const Home = ({ data }) => (
+  <Layout>
+    <Jumbotron fluid className="d-flex justify-content-center">
+      <p className="lead align-self-center">
+        {data.site.siteMetadata.description}
+      </p>
+    </Jumbotron>
+    <Story images={data.images} />
+  </Layout>
+)
 
 export default Home
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,33 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { graphql } from "gatsby"
 import SEO from "../components/seo"
 import Home from "../components/home"
 
 // <SEO title="Home" />
-const IndexPage = () => <Home>content</Home>
+const IndexPage = ({ data }) =>
+  console.log("data=", data) || <Home data={data} />
+
+export const query = graphql`
+  query home {
+    site {
+      siteMetadata {
+        description
+      }
+    }
+    images: allFile(filter: { relativePath: { regex: "/story/" } }) {
+      edges {
+        node {
+          id
+          name
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
