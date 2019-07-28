db92057bcc290b53d2f1236cf301677d79cd06ab
Yasser Zadeh <yassermzh@gmail.com>
2019-07-28 22:32:53 +0200

    change home

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,29 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import Jumbotron from "react-bootstrap/Jumbotron"
 import Layout from "./layout"
 import Story from "./story"
+import seesaw from "../gifs/seesaw-2.gif"
 
 const Home = ({ data }) => (
   <Layout>
-    <Jumbotron fluid className="d-flex justify-content-center">
-      <p className="lead align-self-center">
-        {data.site.siteMetadata.description}
-      </p>
+    <Jumbotron fluid>
+      <Container className="align-items-center justify-content-center">
+        <Row>
+          <Col className="d-flex  justify-content-center">
+            <img src={seesaw} className="home-seesaw" />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2 className="text-primary align-self-center text-center">
+              {data.site.siteMetadata.description}
+            </h2>
+          </Col>
+        </Row>
+      </Container>
     </Jumbotron>
     <Story images={data.images} />
   </Layout>
