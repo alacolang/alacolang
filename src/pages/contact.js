fbcec3ac1356b7fdb0f5276291a951a6368434a8
Yasser Zadeh <yassermzh@gmail.com>
2019-08-01 23:38:10 +0200

    add contract form styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,32 +1,37 @@
 import React from "react"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import Container from "react-bootstrap/Container"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const Contact = () => (
-  <form
-    name="contact"
-    method="post"
-    netlify-honeypot="bot-field"
-    data-netlify="true"
-  >
-    <input type="hidden" name="bot-field" />
-    <label>
-      Name
-      <input type="text" name="name" id="name" />
-    </label>
-    <label>
-      Email
-      <input type="email" name="email" id="email" />
-    </label>
-    <label>
-      Subject
-      <input type="text" name="subject" id="subject" />
-    </label>
-    <label>
-      Message
-      <textarea name="message" id="message" rows="5" />
-    </label>
-    <button type="submit">Send</button>
-    <input type="reset" value="Clear" />
-  </form>
+  <>
+    <SEO title="الاکلنگ: معرفی کتاب" />
+    <Layout>
+      <Container fluid className="contact">
+        <Form
+          name="contact"
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
+          <input type="hidden" name="bot-field" />
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>ایمیل شما</Form.Label>
+            <Form.Control type="email" placeholder="ایمیل خود را وارد کنید" />
+          </Form.Group>
+          <Form.Group controlId="message">
+            <Form.Label>درخواست شما</Form.Label>
+            <Form.Control as="textarea" rows="3" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            ارسال
+          </Button>
+        </Form>
+      </Container>
+    </Layout>
+  </>
 )
 
 export default Contact
