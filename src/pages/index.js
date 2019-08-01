4439fad968fa23748bfbaa977992e246fce1cc40
Yasser Zadeh <yassermzh@gmail.com>
2019-08-01 19:32:21 +0200

    refactor home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,36 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Jumbotron from "react-bootstrap/Jumbotron"
 import SEO from "../components/seo"
-import Home from "../components/home"
+import Layout from "../components/layout"
+import Story from "../components/story"
+import seesaw from "../gifs/seesaw-2.gif"
 
 const IndexPage = ({ data }) => (
   <>
     <SEO title="الاکلنگ" />
-    <Home data={data} />
+    <Layout>
+      <Jumbotron fluid>
+        <Container className="align-items-center justify-content-center">
+          <Row>
+            <Col className="d-flex  justify-content-center">
+              <img src={seesaw} className="home-seesaw" />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h2 className="text-primary align-self-center text-center">
+                {data.site.siteMetadata.description}
+              </h2>
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
+      <Story images={data.images} />
+    </Layout>
   </>
 )
 
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Helmet from "react-helmet"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import get from "lodash/get"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
