b5cec2fe106f3d2c698d0a1cae2d6ebeb61ad703
Yasser Zadeh <yassermzh@gmail.com>
2019-08-01 23:12:23 +0200

    add form name to contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,32 @@
-import React from 'react';
-
+import React from "react"
 
 const Contact = () => (
-  <form method="post" netlify-honeypot="bot-field" data-netlify="true">
-  <label>
-    Name
-    <input type="text" name="name" id="name" />
-  </label>
-  <label>
-    Email
-    <input type="email" name="email" id="email" />
-  </label>
-  <label>
-    Subject
-    <input type="text" name="subject" id="subject" />
-  </label>
-  <label>
-    Message
-    <textarea name="message" id="message" rows="5" />
-  </label>
-  <button type="submit">Send</button>
-  <input type="reset" value="Clear" />
-</form>
+  <form
+    name="contact"
+    method="post"
+    netlify-honeypot="bot-field"
+    data-netlify="true"
+  >
+    <input type="hidden" name="bot-field" />
+    <label>
+      Name
+      <input type="text" name="name" id="name" />
+    </label>
+    <label>
+      Email
+      <input type="email" name="email" id="email" />
+    </label>
+    <label>
+      Subject
+      <input type="text" name="subject" id="subject" />
+    </label>
+    <label>
+      Message
+      <textarea name="message" id="message" rows="5" />
+    </label>
+    <button type="submit">Send</button>
+    <input type="reset" value="Clear" />
+  </form>
 )
 
 export default Contact
