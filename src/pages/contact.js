0f7a29aed43135a9e0a74cbfde9a1771d09d900e
Yasser Zadeh <yassermzh@gmail.com>
2019-08-01 23:07:06 +0200

    try adding contact form

---
+++ src/pages/contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+const Contact = () => (
+  <form method="post" netlify-honeypot="bot-field" data-netlify="true">
+  <label>
+    Name
+    <input type="text" name="name" id="name" />
+  </label>
+  <label>
+    Email
+    <input type="email" name="email" id="email" />
+  </label>
+  <label>
+    Subject
+    <input type="text" name="subject" id="subject" />
+  </label>
+  <label>
+    Message
+    <textarea name="message" id="message" rows="5" />
+  </label>
+  <button type="submit">Send</button>
+  <input type="reset" value="Clear" />
+</form>
+)
+
+export default Contact
